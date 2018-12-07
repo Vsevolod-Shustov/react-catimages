3d974b397caeef068122f45f23413859accb2479
Vsevolod-Shustov <vsevolod.shustov@gmail.com>
2018-12-07 17:44:27 +0300

    square grid

--- src/image/Image.js
+++ src/image/Image.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import transparent from './transparent.png';
 import './Image.css';
 
 class Image extends Component {
   render() {
     return (
-      <div class="image">
-        <img src={this.props.url} />
+      <div className="image" style={{
+        backgroundImage: 'url(' + this.props.url + ')',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}>
+        <img src={transparent} alt="" />
       </div>
     );
   }
