cf465ad64d8bdf691fd28515888ea80d1ff5709c
Vsevolod-Shustov <vsevolod.shustov@gmail.com>
2018-12-07 16:36:42 +0300

    images fetching and rendering, basic grid

---
+++ src/image/Image.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Image.css';
+
+class Image extends Component {
+  render() {
+    return (
+      <div class="image">
+        <img src={this.props.url} />
+      </div>
+    );
+  }
+}
+
+export default Image;
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Main.css';
+import Image from '../image/Image.js';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.catimages_url = "https://api.thecatapi.com/v1/images/search?mime_types=jpg&limit=12";
+    this.state = {
+      images: []
+    };
+  }
+  
+  getImages(url) {
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    // .then(function(jsonData){
+      // return JSON.stringify(jsonData);
+    // })
+    .then(json => {
+      this.setState({ images: json });
+      console.log(json);
+    });
+  }
+  
   render() {
+    let images = this.state.images.map((image) => <Image url={image.url} />);
     return (
       <div id="main">
-      main
-      <img src={logo} className="main-loading" alt="now loading" />
+        {images}
       </div>
     );
+  }
+  
+  componentDidMount() {
+    this.getImages(this.catimages_url);
   }
 }
 
