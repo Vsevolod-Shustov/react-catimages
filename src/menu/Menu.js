f4a9a300b29e33043dfb1c126690cc8a6acb5cba
Vsevolod-Shustov <vsevolod.shustov@gmail.com>
2018-12-13 15:54:14 +0300

    better mobile menu toggle

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -44,24 +44,19 @@ class Menu extends Component {
   }
   
   handleClick() {
-    let e = document.getElementById('menu_items_wrapper_wrapper');
-    if(window.getComputedStyle(e).getPropertyValue("display") === "none") {
-      e.style.display = "block"
-    } else {
-      e.style.display = "none"
-    }
+    document.getElementById('menu_items_wrapper').classList.toggle("menu_mobile_toggled_on");
   }
   
   render() {
     let menu = this.state.menuItems.map((item) => <a className="menu_item" href={item.url} target="_blank" rel="noopener noreferrer">{item.text}</a>);
     return (
       <div id="menu">
-        <div id="menu_items_wrapper_wrapper">
-          <div id="menu_items_wrapper">
-            {menu}
-          </div>
+        <div id="menu_items_wrapper">
+          {menu}
         </div>
-        <div id="menu_toggle" onClick={this.handleClick}>Toggle Menu</div>
+        <div id="menu_toggle" onClick={this.handleClick}>
+          Toggle Menu
+        </div>
       </div>
     );
   }
